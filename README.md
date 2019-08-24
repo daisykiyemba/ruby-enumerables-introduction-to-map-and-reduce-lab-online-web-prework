6da2f0739d093fb5aa7206d80cdb354705ea6979
daisy Kiyemba <deedaisy29@gmail.com>
2019-08-24 15:31:53 +0000

    Automatically backed up by Learn

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Introduction to Map and Reduce Lab
+duce# Introduction to Map and Reduce Lab
 
 ## Learning Goals
 
--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,72 @@
 # My Code here....
+def map_to_negativize(source_array)
+  source_array = [-1, -2, -3, 9]
+  
+end
+
+
+def map_to_no_change(source_array)
+  source_array = ["paul", "gurney", "vladimir", "jessica", "chani"]
+  
+end
+
+def map_to_double(source_array)
+  source_array = [2, 4, 6, -18]
+  
+  
+end
+
+
+def map_to_square(source_array)
+  source_array = [1, 4, 9, 81]
+  
+end
+
+
+def reduce_to_total(source_array, starting_point)
+  source_array = 6
+  starting_point = 106
+  
+end
+
+ def reduce_to_all_true(source_array)
+   
+   source_array = [1,2, true, "razmatazz"]
+   source_array = true
+   
+end
+
+def reduce_to_all_true(source_array)
+  
+   source_array = [1,2, true, "razmatazz", false]
+  source_array = true
+end
+ 
+def reduce_to_any_true(source_array)
+  
+  source_array = [false, nil, nil, nil]
+  source_array = false
+  
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
